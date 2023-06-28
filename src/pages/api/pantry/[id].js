fa59adbe5ddf6bd36263c88101e4d1437382781f
cdpaiva <cdpaivaa@gmail.com>
2023-06-28 12:50:10 -0400

    Adds increment/decrement items

--- src/pages/api/pantry/[id].js
+++ src/pages/api/pantry/[id].js
@@ -3,6 +3,7 @@ import dbConnect from "../../../lib/dbConnect";
 import jwt from "jsonwebtoken";
 
 export default async function handler(req, res) {
+  await dbConnect();
   console.log(req.query);
   const {
     query: { id },
